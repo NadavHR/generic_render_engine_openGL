941c7a41552efe9c3a90f33eb1e14b876070b7a8
NadavHR <nadavh2611@gmail.com>
2025-06-12 09:25:02 +0300

    added tone mapper shader

--- src/headers/shading/shaders/default_shaders.hpp
+++ src/headers/shading/shaders/default_shaders.hpp
@@ -2,11 +2,12 @@
 #define DEFAULT_SHADERS_HPP
 #include "render_shader.hpp"
 
-#define PATH_TO_SHADER(shader) ("/assets/shaders/" shader)
+#define PATH_TO_SHADER(shader) ("./assets/shaders/" shader)
 
 namespace DefaultShaders {
     extern RenderShader *defferedPointLight;
     extern RenderShader *modelRenderDeferredHDR;
+    extern RenderShader *toneMapper;
     void initializeShaders();
 }
 
--- src/sources/default_shaders.cpp
+++ src/sources/default_shaders.cpp
@@ -1,7 +1,7 @@
 #include "default_shaders.hpp"
 RenderShader *DefaultShaders::defferedPointLight = NULL;
 RenderShader *DefaultShaders::modelRenderDeferredHDR = NULL;
-
+RenderShader *DefaultShaders::toneMapper = NULL;
 void DefaultShaders::initializeShaders() {
     BasicShader noChangesVertex(PATH_TO_SHADER("no_changes.vs"), GL_VERTEX_SHADER);
     BasicShader defferedPointLightFragment(PATH_TO_SHADER("deffered_point_light.fs"), GL_FRAGMENT_SHADER);
@@ -10,4 +10,7 @@ void DefaultShaders::initializeShaders() {
     BasicShader modelTransformVertex(PATH_TO_SHADER("model_transform.vs"), GL_VERTEX_SHADER);
     BasicShader modelRenderDeferredHDRFragment(PATH_TO_SHADER("model_render_deferred_HDR.fs"), GL_FRAGMENT_SHADER);
     modelRenderDeferredHDR = new RenderShader(modelTransformVertex, modelRenderDeferredHDRFragment);
+
+    BasicShader toneMapperFragment(PATH_TO_SHADER("tone_mapper.fs"), GL_FRAGMENT_SHADER);
+    toneMapper = new RenderShader(noChangesVertex, toneMapperFragment);
 }
