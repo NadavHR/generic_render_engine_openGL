38a5f1540e9e8ef6caf1359c8097b917b7501847
NadavHR <nadavh2611@gmail.com>
2025-06-17 09:46:26 +0300

    added guards to avoid reinitialization

--- src/sources/default_shaders.cpp
+++ src/sources/default_shaders.cpp
@@ -7,17 +7,25 @@ RenderShader *DefaultShaders::toneMapper = NULL;
 void DefaultShaders::initializeShaders() {
     BasicShader noChangesVertex(PATH_TO_SHADER("no_changes.vs"), GL_VERTEX_SHADER);
     BasicShader defferedPointLightFragment(PATH_TO_SHADER("deffered_point_light.fs"), GL_FRAGMENT_SHADER);
-    deferredPointLight = new RenderShader(noChangesVertex, defferedPointLightFragment);
+    // only initialize if not already initialized
+    if (!deferredPointLight) {
+        deferredPointLight = new RenderShader(noChangesVertex, defferedPointLightFragment);
+    }
 
     BasicShader defferedAmbientLightFragment = BasicShader(PATH_TO_SHADER("ambient_light_deferred.fs"), GL_FRAGMENT_SHADER);
-    deferredAmbientLight = new RenderShader(noChangesVertex, defferedAmbientLightFragment);
-
+    if (!deferredAmbientLight) {
+        deferredAmbientLight = new RenderShader(noChangesVertex, defferedAmbientLightFragment);
+    }
     BasicShader modelTransformVertex(PATH_TO_SHADER("model_transform.vs"), GL_VERTEX_SHADER);
     BasicShader modelRenderDeferredHDRFragment(PATH_TO_SHADER("model_render_deferred_HDR.fs"), GL_FRAGMENT_SHADER);
-    modelRenderDeferredHDR = new RenderShader(modelTransformVertex, modelRenderDeferredHDRFragment);
+    if (!modelRenderDeferredHDR) {
+        modelRenderDeferredHDR = new RenderShader(modelTransformVertex, modelRenderDeferredHDRFragment);
+    }
 
     BasicShader toneMapperFragment(PATH_TO_SHADER("tone_mapper.fs"), GL_FRAGMENT_SHADER);
-    toneMapper = new RenderShader(noChangesVertex, toneMapperFragment);
+    if (!toneMapper) {
+        toneMapper = new RenderShader(noChangesVertex, toneMapperFragment);
+    }
 }
 
 void DefaultShaders::setViewForShaders(glm::vec3 const viewPose, glm::mat4 const viewRotation)
